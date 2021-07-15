72f1c0c2eb60ed07da24f0bb5b08f331539a53c6
LuFipe <luizfelipecastrocoelho@gmail.com>
2021-07-15 11:15:57 -0300

    -FORMS + POSTS
    -Implementado .pug files e .html para paginas dinamicas e estaticas
    -formos com o metodo post adicionados aos formularios nos .pug files

--- HelloCalang/routes/roteador.js
+++ HelloCalang/routes/roteador.js
@@ -1,31 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
+//Auxilio para direcionamento
+function Caminho(caminho){
+	return path.normalize(__dirname +'/../' + caminho)
+};
 
 //Get index
 router.get('/',
 	(req,res,next)=>{
-		res.render('index');
+		res.sendFile(Caminho('FrontEnd/HTML/index.html'));
 	}
 );
 
 //Get pagina cadastro.html
 router.get('/cadastro', 
 	(req,res,next)=>{
-		res.render('cadastro')
+		res.render('cadastrar')
 	}
 );
+//Post pagina cadastro.pug
+router.post('/cadastro',
+	(req,res,next)=>{
+		var dados = {'NOME':req.body.NOME, 'DATA': req.body.DATA, 'ENDERECO': req.body.ENDERECO, 'CPF':req.body.CPF, 'RG': req.body.RG}
+		res.render('cadastro', dados);
+	}
+)
 
-//Get pagina busca.html
+//Get pagina buscar.html
 router.get('/busca', 
 	(req,res,next)=>{
-		res.render('busca');
+		res.sendFile(Caminho('/FrontEnd/HTML/buscar.html'))
 	}
 );
+//Post pagina buscar.pug
+
 
 //Get pagina excluir.html
 router.get('/excluir', 
 	(req,res,next)=>{
-		res.send(" EXCLUIR Not implemented yet");
+		res.render('buscar');
 	}
 );
 
